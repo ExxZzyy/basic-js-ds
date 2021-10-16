aac3e2a13f8da6be6d1629416f6286b36b6f1503
ExxZzyy <66119764+ExxZzyy@users.noreply.github.com>
2021-10-16 20:08:34 +0300

    feat: solve all

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,93 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  constructor() {
+    this.tree = null
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.tree = addNode(this.tree, data);
+    function addNode(node, data) {
+      if (!node) return new Node(data);
+      if (node.data === data) return node;
+      if (data > node.data) node.right = addNode(node.right, data);
+      else node.left = addNode(node.left, data);
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+      function hasNode(node, data) {
+        if (!node) return false;
+        if (node.data === data) return true;
+        if (data > node.data) return hasNode(node.right, data);
+        else return hasNode(node.left, data);
+      }
+    return hasNode(this.tree, data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function findNode(node, data) {
+      if (!node) return null;
+      if (node.data === data) return node;
+      if (data > node.data) return findNode(node.right, data);
+      else return findNode(node.left, data);
+    }
+    return findNode(this.tree, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.root = removeNode(this.tree, data)
+    function removeNode(node, data) {
+      if (!node) return null;
+      if (data > node.data) {
+        node.right = removeNode(node.right, data)
+        return node
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data)
+        return node
+      } else {
+        if (!node.left && !node.right) {
+          return null
+        }
+        if (!node.left) {
+          node = node.right
+          return node
+        }
+        if (!node.right) {
+          node = node.left
+          return node
+        }
+        let rightMin = node.right
+        while (rightMin.left) {
+          rightMin = rightMin.left
+        }
+        node.data = rightMin.data
+        node.right = removeNode(node.right, rightMin.data)
+        return node
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.tree;
+    if (!node) return;
+    while (node.left) {
+      node = node.left
+    }
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.tree;
+    if (!node) return;
+    while (node.right) {
+      node = node.right
+    }
+    return node.data
   }
 
 }
--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  while (l && l.value === k) l = l.next;
+  let current = l;
+  while (current.next) {
+    if(k === current.next.value) current.next = current.next.next;
+    current = current.next;
+  }
+  return l;
 }
